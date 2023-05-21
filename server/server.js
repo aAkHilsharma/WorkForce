6002427f14ddd7cdb3bdde2b797c9e08db1320c3
Akhil Sharma <akhilsharma6721@gmail.com>
2023-05-21 20:56:56 +0530

    initial commit

--- server/server.js
+++ server/server.js
@@ -1,5 +1,9 @@
 const express = require('express');
+require('dotenv').config();
+const dbConfig = require('./config/dbConfig');
 const app = express();
-const port = process.env.PORT || 5000;
 
+app.use(express.json());
+
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Node server listening on port ${port}`));
