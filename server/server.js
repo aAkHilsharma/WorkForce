1803ce7fbbdb58def95fbaf7a8a0002d4b4793a1
Akhil Sharma <akhilsharma6721@gmail.com>
2023-06-02 22:13:33 +0530

    added body parser

--- server/server.js
+++ server/server.js
@@ -2,8 +2,12 @@ const express = require('express');
 require('dotenv').config();
 const dbConfig = require('./config/dbConfig');
 const app = express();
+const bodyParser = require('body-parser');
+const usersRoute = require('./routes/usersRoute');
 
 app.use(express.json());
+
+app.use('/api/users', usersRoute);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Node server listening on port ${port}`));
