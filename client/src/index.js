7fb184dff3f2c4f41e8939431276ba6004f4b152
Akhil Sharma <akhilsharma6721@gmail.com>
2023-06-03 11:52:27 +0530

    added state management for users

--- client/src/index.js
+++ client/src/index.js
@@ -3,19 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 import { ConfigProvider } from 'antd';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <ConfigProvider
-    theme={{
-      token: {
-        colorPrimary: '#2E3840',
-        colorBorder: '#2E3840',
-      },
-    }}
-  >
-    <App />
-  </ConfigProvider>
+  <Provider store={store}>
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: '#2E3840',
+          colorBorder: '#2E3840',
+        },
+      }}
+    >
+      <App />
+    </ConfigProvider>
+  </Provider>
 );
 reportWebVitals();
