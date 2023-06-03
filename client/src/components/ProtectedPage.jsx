28878a5832a956d33143f9c49f65ab01f39a65ca
Akhil Sharma <akhilsharma6721@gmail.com>
2023-06-03 12:48:23 +0530

    added loaders and user from state

--- client/src/components/ProtectedPage.jsx
+++ client/src/components/ProtectedPage.jsx
@@ -1,21 +1,27 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { GetLoggedInUser } from '../apicalls/users';
+import { useDispatch, useSelector } from 'react-redux';
+import { SetUser } from '../redux/usersSlice';
+import { SetLoading } from '../redux/loadersSlice';
 
 const ProtectedPage = ({ children }) => {
   const navigate = useNavigate();
-  const [user, setUser] = useState(null);
+  const { user } = useSelector((state) => state.users);
+  const dispatch = useDispatch();
   useEffect(() => {
     const getUser = async () => {
       try {
+        dispatch(SetLoading(true));
         const response = await GetLoggedInUser();
-        console.log(response.data);
+        dispatch(SetLoading(false));
         if (response.success) {
-          setUser(response.data);
+          dispatch(SetUser(response.data));
         } else {
           throw new Error(response.message);
         }
       } catch (error) {
+        dispatch(SetLoading(false));
         alert(error.message);
         localStorage.removeItem('token');
         navigate('/login');
@@ -26,14 +32,31 @@ const ProtectedPage = ({ children }) => {
     } else {
       navigate('/login');
     }
-  }, [navigate]);
+  }, [dispatch, navigate]);
   return (
     <div>
-      <h1>ProtectedPage</h1>
-      <h1>
-        Welcome {user?.firstName} {user?.lastName}{' '}
-      </h1>
-      {children}
+      <div className='flex justify-between items-center bg-primary text-white px-5 py-4'>
+        <h1 className='text-2xl'>Work-Force</h1>
+        <div className='flex items-center bg-white px-5 py-2 rounded'>
+          <span
+            className='text-primary underline cursor-pointer'
+            onClick={() => {
+              navigate('/profile');
+            }}
+          >
+            {user && user?.firstName}
+          </span>
+          <i className='ri-notification-2-line text-white bg-gray-500 mx-2 rounded-full p-2 cursor-pointer'></i>
+          <i
+            className='ml-10 ri-logout-box-r-line text-primary cursor-pointer'
+            onClick={() => {
+              localStorage.removeItem('token');
+              navigate('/login');
+            }}
+          ></i>
+        </div>
+      </div>
+      <div className='px-5 py-3'>{children}</div>
     </div>
   );
 };
