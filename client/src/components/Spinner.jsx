3acf4a4179bf9c1b98c4eac01bb1f94ad2c95371
Akhil Sharma <akhilsharma6721@gmail.com>
2023-06-04 11:24:36 +0530

    updated z index

--- client/src/components/Spinner.jsx
+++ client/src/components/Spinner.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Spinner = () => {
   return (
-    <div className='fixed inset-0 flex justify-center items-center bg-black opacity-70 z-40'>
+    <div className='fixed inset-0 flex justify-center items-center bg-black opacity-70 z-[99999]'>
       <div className='h-10 w-10 border-solid border-3 border-gray-200 border-l-transparent rounded-full animate-spin'></div>
     </div>
   );
