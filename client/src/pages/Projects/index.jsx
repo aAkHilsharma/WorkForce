1bc35ee321a56b6351edbf239a50c330412ea66c
Akhil Sharma <akhilsharma6721@gmail.com>
2023-06-04 11:38:40 +0530

    added api call to get projects

--- client/src/pages/Projects/index.jsx
+++ client/src/pages/Projects/index.jsx
@@ -1,9 +1,69 @@
-import { Button } from 'antd';
-import React, { useState } from 'react';
+import { Button, Table } from 'antd';
+import React, { useCallback, useEffect, useState } from 'react';
 import ProjectForm from './ProjectForm';
+import { useDispatch, useSelector } from 'react-redux';
+import { SetLoading } from '../../redux/loadersSlice';
+import { GetAllProjects } from '../../apicalls/project';
+import { getDateFormat } from '../../utils/helper';
+
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+  },
+  {
+    title: 'Description',
+    dataIndex: 'description',
+  },
+  {
+    title: 'Status',
+    dataIndex: 'status',
+  },
+  {
+    title: 'CreatedAt',
+    dataIndex: 'createdAt',
+    render: (text) => getDateFormat(text),
+  },
+  {
+    title: 'Action',
+    dataIndex: 'action',
+    render: (text, record) => {
+      return (
+        <div className='flex gap-5'>
+          <i className='ri-delete-bin-line cursor-pointer'></i>
+          <i className='ri-pencil-line cursor-pointer'></i>
+        </div>
+      );
+    },
+  },
+];
 
 const Projects = () => {
   const [show, setShow] = useState(false);
+  const [projects, setProjects] = useState([]);
+  const { user } = useSelector((state) => state.users);
+  const dispatch = useDispatch();
+
+  const getData = useCallback(async () => {
+    try {
+      dispatch(SetLoading(true));
+      const response = await GetAllProjects({ owner: user?._id });
+      if (response.success) {
+        setProjects(response.data);
+      } else {
+        throw new Error(response.error);
+      }
+      dispatch(SetLoading(false));
+    } catch (error) {
+      dispatch(SetLoading(false));
+      alert(error.message);
+    }
+  }, [dispatch, user?._id]);
+
+  useEffect(() => {
+    getData();
+  }, [getData]);
+
   return (
     <div>
       <div className='flex justify-end'>
@@ -15,8 +75,10 @@ const Projects = () => {
           Add Project
         </Button>
       </div>
-
-      {show && <ProjectForm show={show} setShow={setShow} />}
+      <Table columns={columns} dataSource={projects} className='mt-4' />
+      {show && (
+        <ProjectForm show={show} setShow={setShow} reloadData={getData} />
+      )}
     </div>
   );
 };
