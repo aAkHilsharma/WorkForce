1d4177ecf92d56f6648fbcc9fb0ce1ab843f6740
Akhil Sharma <akhilsharma6721@gmail.com>
2023-06-04 11:27:52 +0530

    added date format function

--- client/src/utils/helper.jsx
+++ client/src/utils/helper.jsx
@@ -1,6 +1,12 @@
+import moment from 'moment';
+
 export const antdFormRules = [
   {
     required: true,
     message: 'Required',
   },
 ];
+
+export const getDateFormat = (date) => {
+  return moment(date).format('MMM Do YYYY h:mm a');
+};
