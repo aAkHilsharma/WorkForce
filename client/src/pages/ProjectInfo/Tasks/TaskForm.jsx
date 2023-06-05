441e7921987ee9d1c322c666c7d1320b62fe9a48
Akhil Sharma <akhilsharma6721@gmail.com>
2023-06-05 20:36:54 +0530

    added tasks and task form component

---
+++ client/src/pages/ProjectInfo/Tasks/TaskForm.jsx
@@ -0,0 +1,98 @@
+import { useRef, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Form, Input, Modal } from 'antd';
+import TextArea from 'antd/es/input/TextArea';
+import { CreateTask } from '../../../apicalls/tasks';
+import { SetLoading } from '../../../redux/loadersSlice';
+
+const TaskForm = ({
+  showTaskForm,
+  setShowTaskForm,
+  project,
+  task,
+  reloadData,
+}) => {
+  const [email, setEmail] = useState('');
+  const formRef = useRef(null);
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.users);
+
+  const onFinish = async (values) => {
+    try {
+      let response = null;
+      dispatch(SetLoading(true));
+      if (task) {
+      } else {
+        const assignedToMember = project.members.find(
+          (member) => member.user.email === email
+        );
+        const assignedToUserId = assignedToMember.user._id;
+        const assignedBy = user._id;
+
+        response = await CreateTask({
+          ...values,
+          project: project._id,
+          assignedTo: assignedToUserId,
+          assignedBy,
+        });
+      }
+      if (response.success) {
+        reloadData();
+        alert(response.message);
+        setShowTaskForm(false);
+      }
+      dispatch(SetLoading(false));
+    } catch (error) {
+      dispatch(SetLoading(false));
+      alert(error.message);
+    }
+  };
+
+  const validateEmail = () => {
+    const employeesInProject = project.members.filter(
+      (member) => member.role === 'employee'
+    );
+    const isEmailValid = employeesInProject.find(
+      (employee) => employee.user.email === email
+    );
+    return isEmailValid ? true : false;
+  };
+
+  return (
+    project && (
+      <Modal
+        title='ADD TASK'
+        open={showTaskForm}
+        onCancel={() => setShowTaskForm(false)}
+        centered
+        onOk={() => {
+          formRef.current.submit();
+        }}
+      >
+        <Form layout='vertical' ref={formRef} onFinish={onFinish}>
+          <Form.Item label='Task Name' name='name'>
+            <Input />
+          </Form.Item>
+          <Form.Item label='Description' name='description'>
+            <TextArea />
+          </Form.Item>
+          <Form.Item label='Assign To' name='assignedTo'>
+            <Input
+              placeholder='Enter email of the employee'
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </Form.Item>
+          {email && !validateEmail() && (
+            <div className='bg-red-700 text-sm p-2'>
+              <span className='text-white'>
+                Email is not valid or employee in the project
+              </span>
+            </div>
+          )}
+        </Form>
+      </Modal>
+    )
+  );
+};
+
+export default TaskForm;
--- client/src/pages/ProjectInfo/Tasks/index.jsx
+++ client/src/pages/ProjectInfo/Tasks/index.jsx
@@ -1,7 +1,101 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
+import { Button, Table } from 'antd';
+import TaskForm from './TaskForm';
+import { useDispatch, useSelector } from 'react-redux';
+import { SetLoading } from '../../../redux/loadersSlice';
+import { GetAllTasks } from '../../../apicalls/tasks';
+import { getDateFormat } from '../../../utils/helper';
 
-const Tasks = () => {
-  return <div>Tasks</div>;
+const Tasks = ({ project }) => {
+  const [showTaskForm, setShowTaskForm] = useState(false);
+  const [tasks, setTask] = useState(null);
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.users);
+
+  const getTasks = useCallback(async () => {
+    try {
+      dispatch(SetLoading(true));
+      const response = await GetAllTasks({
+        project: project._id,
+      });
+      dispatch(SetLoading(false));
+      if (response.success) {
+        setTask(response.data);
+      } else {
+        throw new Error(response.message);
+      }
+    } catch (error) {
+      dispatch(SetLoading(false));
+      alert(error.message);
+    }
+  }, [dispatch, project._id]);
+
+  useEffect(() => {
+    getTasks();
+  }, [getTasks]);
+
+  const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+    },
+    {
+      title: 'Assigned To',
+      dataIndex: 'assignedTo',
+      render: (text, record) =>
+        record.assignedTo.firstName + ' ' + record.assignedTo.lastName,
+    },
+    {
+      title: 'Assigned By',
+      dataIndex: 'assignedBy',
+      render: (text, record) =>
+        record.assignedBy.firstName + ' ' + record.assignedBy.lastName,
+    },
+    {
+      title: 'Assigned On',
+      dataIndex: 'createdAt',
+      render: (text, record) => getDateFormat(text),
+    },
+    {
+      title: 'Status',
+      dataIndex: 'status',
+      render: (text, record) => text.toUpperCase(),
+    },
+    {
+      title: 'action',
+      dataIndex: 'action',
+    },
+  ];
+
+  const isEmployee = project.members.find(
+    (member) => member.role === 'employee' && member.user._id === user._id
+  );
+
+  return (
+    <div>
+      <div className='flex justify-end'>
+        {!isEmployee && (
+          <Button
+            onClick={() => {
+              setShowTaskForm(true);
+            }}
+          >
+            Add Task
+          </Button>
+        )}
+      </div>
+      <Table className='mt-4' columns={columns} dataSource={tasks} />
+
+      {showTaskForm && (
+        <TaskForm
+          showTaskForm={showTaskForm}
+          setShowTaskForm={setShowTaskForm}
+          project={project}
+          reloadData={getTasks}
+        />
+      )}
+    </div>
+  );
 };
 
 export default Tasks;
