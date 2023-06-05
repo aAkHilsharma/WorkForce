e50afd66e5db41f6a44eab8d800c669711111ed4
Akhil Sharma <akhilsharma6721@gmail.com>
2023-06-05 20:33:13 +0530

    added tasks route

--- server/server.js
+++ server/server.js
@@ -6,11 +6,13 @@ const bodyParser = require('body-parser');
 
 const usersRoute = require('./routes/usersRoute');
 const projectRoute = require('./routes/projectRoute');
+const taskRoute = require('./routes/tasksRoute');
 
 app.use(express.json());
 
 app.use('/api/users', usersRoute);
 app.use('/api/projects', projectRoute);
+app.use('/api/tasks', taskRoute);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Node server listening on port ${port}`));
